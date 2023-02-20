d32e9c8730c141aa9e12bef410083b918ea8462d
Muhammad Asif Adnan <asif.adnan.cse@gmail.com>
2023-02-20 10:44:10 +0600

    feat: add analytics on people fetch

--- src/pages/people/[id]/index.tsx
+++ src/pages/people/[id]/index.tsx
@@ -25,33 +25,59 @@ const SWAPI = "https://swapi.dev/api";
 
 const People: React.FC = () => {
   const [person, setPerson] = React.useState<IPerson | null>(null);
+  const [fetchId, setFetchId] = React.useState<number | null>(null);
   const router = useRouter();
   const { id } = router.query;
 
   React.useEffect(() => {
+    if (id) {
+      setFetchId(+id);
+    }
+  }, [id]);
+
+  React.useEffect(() => {
     async function fetchPeople() {
       try {
-        const res = await axios.get(`${SWAPI}/people/${id}`);
+        const res = await axios.get(`${SWAPI}/people/${fetchId}`);
         const data = res.data;
         setPerson(data);
+
+        const event = {
+          event: "Custom Event",
+          eventCategory: "category",
+          eventAction: "action",
+          eventLabel: "label",
+          eventValue: fetchId,
+        };
+        if (
+          typeof window !== "undefined" &&
+          typeof window.dataLayer !== "undefined"
+        ) {
+          window.dataLayer.push(event);
+        }
       } catch (err) {
         console.log(err);
       }
     }
 
-    if (id) {
+    if (fetchId) {
       fetchPeople();
     }
-  }, [id]);
+  }, [fetchId]);
 
   if (!person) {
     return <span>loading...</span>;
   }
 
+  const handleNext = () => {
+    setFetchId((f) => (f ? f + 1 : 0));
+  };
+
   return (
     <div>
       <h1>{person?.name}</h1>
       <span>{person?.eye_color} eyes</span>
+      <button onClick={handleNext}>Next</button>
     </div>
   );
 };
